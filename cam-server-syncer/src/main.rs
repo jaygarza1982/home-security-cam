f2cda974aa5ec54a2f278a1c3415552c2c3848c4
Jake Garza <jaygarza1982@gmail.com>
2022-07-24 15:29:21 -0400

    Fixed rocket server not running in Docker

--- cam-server-syncer/src/main.rs
+++ cam-server-syncer/src/main.rs
@@ -1,21 +1,17 @@
-// #[macro_use] extern crate rocket;
+#[macro_use] extern crate rocket;
 
-// #[get("/")]
-// fn index() -> &'static str {
-//     "Hello, world!"
-// }
+#[get("/")]
+fn index() -> &'static str {
+    "Hello, world!"
+}
 
-// // TODO: Accept JSON that contains path to file to upload adn the S3 key to upload to
-// // #[post("/upload")]
-// // fn upload() {
+// TODO: Accept JSON that contains path to file to upload adn the S3 key to upload to
+// #[post("/upload")]
+// fn upload() {
     
-// // }
-
-// #[launch]
-// fn rocket() -> _ {
-//     rocket::build().mount("/", routes![index])
 // }
 
-fn main() {
-    println!("Rust hello world!");
+#[launch]
+fn rocket() -> _ {
+    rocket::build().mount("/", routes![index])
 }
